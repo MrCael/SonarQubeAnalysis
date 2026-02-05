4764394602046a1ede7581a04149318ce063f4d3
Cael Erickson <cse37@byu.edu>
2026-02-05 16:56:14 -0700

    Added test function to test the Main class main method returns the right output

--- src/test/java/edu/byu/cs/sonar/MainTest.java
+++ src/test/java/edu/byu/cs/sonar/MainTest.java
@@ -1,17 +1,44 @@
 package edu.byu.cs.sonar;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 class MainTest {
 
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @BeforeEach
+    void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void cleanUpStreams() {
+        System.setOut(originalOut);
+    }
 
     @Test
     void testMain() {
         String[] args = {"readMe1.txt", "readMe2.txt", "readMe3.txt", "5"};
 
         assertDoesNotThrow(() -> Main.main(args));
+    }
 
+    @Test
+    void testMain_testOutput() {
+      String[] args = {"readMe1.txt", "readMe2.txt", "readMe3.txt", "5"};
+      String expected = "was times was It It \n";
+
+      Main.main(args);
+
+      assertEquals(expected, outContent.toString());
     }
 }
